fdd3789d5a83b7da8f704034b26cbf65f3fa1ba5
shreverr <verma2007s@gmail.com>
2022-11-27 17:26:43 +0530

    added naamkaran icon

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
-from tkinter import *
-from tkinter.ttk import *
+from customtkinter import *
+# from tkinter import *
+# from tkinter.ttk import *
 
-#main window named "root"
-
-NKWindow = Tk()
+# main window named "NKWindow"
+set_appearance_mode("dark")
+NKWindow = CTk()
 NKWindow.geometry("928x522")
 NKWindow.title("NaamKaran")
-label = Label(NKWindow, text = "Welcome to the Proagram").pack()
+NKWindow.iconbitmap("NaamKaran.ico")
+
+
 NKWindow.mainloop()
