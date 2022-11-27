977f19227f6acc5f94796b5f53ac22b28f858370
Shivansh Khunger <khunger.shivansh@outlook.com>
2022-11-27 17:26:35 +0530

    Edit - Updated for Custom tkinter

--- ui.py
+++ ui.py
@@ -1,10 +1,17 @@
-from tkinter import *
-from tkinter.ttk import *
+import customtkinter
+
+import tkinter
 
 #main window named "root"
 
-NKWindow = Tk()
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
+NKWindow = customtkinter.CTk()
 NKWindow.geometry("928x522")
-NKWindow.title("NaamKaran")
-label = Label(NKWindow, text = "Welcome to the Proagram").pack()
+NKWindow.title("Naam Karan")
+
+T1 = customtkinter.CTkLabel(NKWindow, text = "Welcome to the Program")    # T1 - 1st Text
+T1.pack()
+
 NKWindow.mainloop()
