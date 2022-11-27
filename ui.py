aa3dd249141de37bca29104259731d02b9ae8e55
Shivansh Khunger <khunger.shivansh@outlook.com>
2022-11-27 17:49:08 +0530

    Edit - Updated for Custom tkinter

--- ui.py
+++ ui.py
@@ -3,18 +3,26 @@ import customtkinter
 
 import tkinter
 
-#main window named "root"
+#main window named "NKWind"
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
-NKWindow = customtkinter.CTk()
-NKWindow.geometry("928x522")
-NKWindow.title("Naam Karan")
+NKWind = customtkinter.CTk()
+NKWind.geometry("928x522")
+NKWind.title("Naam Karan")
+NKWind.iconbitmap("NaamKaran.ico")
 
-T1 = customtkinter.CTkLabel(NKWindow, text = "Welcome to the Program")    # T1 - 1st Text
-T1.pack()
-NKWindow.iconbitmap("NaamKaran.ico")
+# L1 = customtkinter.CTkLabel(NKWind, text ="Welcome to the Program")    # L1 - 1st Text
+# L1.pack()
 
+F1 = customtkinter.CTkFrame(master=NKWind)
+F1.pack(pady=10, padx=60, fill="both", expand = True)
 
-NKWindow.mainloop()
+L2 = customtkinter.CTkLabel(master=F1, text = "First Line",  justify=tkinter.LEFT)
+L2.pack(pady=12, padx=10)
+
+L3 = customtkinter.CTkLabel(master=F1, text = "Second Line",  justify=tkinter.LEFT)
+L3.pack(pady=12, padx=10)
+
+NKWind.mainloop()
