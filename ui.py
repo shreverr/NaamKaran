fd7bded24e03881d9538a73f1edef02e55278d71
Shivansh-Khunger <110467229+Shivansh-Khunger@users.noreply.github.com>
2022-11-27 17:57:53 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -3,9 +3,9 @@ import customtkinter
 
 import tkinter
 
-#main window named "NKWind"
+# Main window named "NKWind"
 
-customtkinter.set_appearance_mode("dark")
+customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("green")
 
 NKWind = customtkinter.CTk()
@@ -13,16 +13,13 @@ NKWind.geometry("928x522")
 NKWind.title("Naam Karan")
 NKWind.iconbitmap("NaamKaran.ico")
 
-L1 = customtkinter.CTkLabel(NKWind, text ="Welcome to the Program")    # L1 - 1st Text
-L1.pack()
-
-F1 = customtkinter.CTkFrame(master=NKWind)
+F1 = customtkinter.CTkFrame(master=NKWind)    # F1 - Main Frame
 F1.pack(pady=10, padx=60, fill="both", expand = True)
 
-L2 = customtkinter.CTkLabel(master=F1, text = "First Line",  justify=tkinter.LEFT)
-L2.pack(pady=12, padx=10)
+L1 = customtkinter.CTkLabel(master=F1, text = "First Line",  justify=tkinter.LEFT)
+L1.pack(pady=12, padx=10)
 
-L3 = customtkinter.CTkLabel(master=F1, text = "Second Line",  justify=tkinter.LEFT)
-L3.pack(pady=12, padx=10)
+L2 = customtkinter.CTkLabel(master=F1, text = "Second Line",  justify=tkinter.LEFT)
+L2.pack(pady=12, padx=10)
 
 NKWind.mainloop()
