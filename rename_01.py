c3ef2a53c1267d9951690aa99806b558fc5b569c
Shivansh Khunger <khunger.shivansh@outlook.com>
2022-12-19 10:10:23 +0530

    x

---
+++ rename_01.py
@@ -0,0 +1,27 @@
+import os
+
+prefix = input()
+folder = r"C:\Users\Khung\Desktop\Python Lab"
+filesRename = os.listdir(folder)  # List of Files
+num_files = len(filesRename)
+# Iterate
+suf = 1 # Taking Suffix as 1
+for file in os.listdir(folder):
+    # Checking if the file is present in the list
+    if file in filesRename:
+        oldName = os.path.join(folder, file)
+        n = os.path.splitext(file)[0]  # Path of the files excluding the Extension
+
+        ext = os.path.splitext(file)[1]  # Extension of the file is stored in this variable
+
+        b = prefix + " " +  str(suf) + ext  # New Name
+
+        suf += 1  # Numbering System
+
+        newName = os.path.join(folder, b)
+
+        # Renaming the file
+        os.rename(oldName, newName)
+
+
+
